81dd5b1b7287b3971913997eb393f4c8385b4544
Ali-codesweb <aliasgar1099@gmail.com>
2021-05-05 01:42:27 +0530

    Removed virtual environment

--- backend/settings.py
+++ backend/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = secret.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['foowoo1.herokuapp.com']
+ALLOWED_HOSTS = ['foowoo1.herokuapp.com','127.0.0.1']
 
 
 # Application definition
